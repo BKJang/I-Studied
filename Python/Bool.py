6673e68b753759184e110a07ed421f30eb5c98d7
BKJang <jp302119@gmail.com>
2017-01-25 15:37:53 +0900

    Upload Bool of Python that I-Studied.

---
+++ Python/Bool.py
@@ -0,0 +1,16 @@
+#-*- coding: utf-8 -*-
+print(3>5) # False
+print(3.14>2) # True
+print('a'=='b') # False
+print(3.14!=3.14) # False
+
+print(True and False) # False
+print(False or True) # True
+print(not False) # True
+print(4 > 5 and 104 < 311.4) # False
+
+print(bool(0)) # False
+print(bool(1)) # True
+print(bool(5.41)) # True
+print(bool([])) # False
+print(bool('a')) # True
