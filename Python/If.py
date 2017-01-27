c43238b88b47985c6559b6021c13b084ebaf143b
BKJang <jp302119@gmail.com>
2017-01-27 17:26:32 +0900

    Upload If of Python I studied.

---
+++ Python/If.py
@@ -0,0 +1,21 @@
+#-*- coding: utf-8 -*-
+money = 1000
+if money >= 500:
+    print("money가 500원 이상입니다.")
+# money가 500원 이상입니다.
+
+money1 = 500
+if money >= 500:
+    print("돈이 500원 이상 있습니다.")
+else :
+    print("돈이 500원 미만입니다.")
+# 돈이 500원 이상 있습니다.
+
+money2 = 500
+if money > 500:
+    print("돈이 500원보다 많습니다.")
+elif money == 500:
+    print("가지고 있는 돈이 500원이 있습니다.")
+elif money <500:
+    print("돈이 500원보다 적습니다.")
+# 돈이 500원보다 많습니다.
