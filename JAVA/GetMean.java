1d01f5824f3d691e1c579c5054a522d37a8ccf28
PaulJ <jp302119@gmail.com>
2017-03-16 09:42:47 +0900

    Upload GetMean.java
    This code is about getting mean of array x.

---
+++ JAVA/GetMean.java
@@ -0,0 +1,21 @@
+/*
+def average(list):
+함수를 완성해서 매개변수 list의 평균값을 return하도록 만들어 보세요.
+어떠한 크기의 list가 와도 평균값을 구할 수 있어야 합니다.
+*/
+public class GetMean {
+    public int getMean(int[] array) {
+      int a = 0;
+      for(int i=0; i<array.length; i++){
+      	a += array[i];
+      }
+      return a/array.length;
+    }
+
+    public static void main(String[] args) {
+        int x[] = {5, 4, 3};
+        GetMean getMean = new GetMean();
+        //main
+        System.out.println("평균값 : " + getMean.getMean(x));
+    }
+}
